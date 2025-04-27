922d953ba922cf18acbf5383a941db49546d8af0
Navyashri-Kolachippu <navya.nsc@gmail.com>
2025-04-27 18:55:59 +0530

    remove multiple api calls

--- src/hooks/useUpComingMovies.js
+++ src/hooks/useUpComingMovies.js
@@ -1,11 +1,11 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { useEffect } from "react";
 import { addUpComingMovies } from "../utils/movieSlice";
-
+ 
 const useUpComingMovies=()=>{
  const dispatch=useDispatch();
-
+ const upComingMoviesData=useSelector((store)=>store.movies.upComingMovies);
  const upComingMovies=async()=>{
     const data= await fetch('https://api.themoviedb.org/3/movie/upcoming?page=1',API_OPTIONS);
     const json= await data.json();
@@ -13,7 +13,9 @@ const useUpComingMovies=()=>{
     dispatch(addUpComingMovies(json.results));
  }
 
-  useEffect(()=>{upComingMovies()},[]);
+  useEffect(()=>{
+   !upComingMoviesData && upComingMovies()
+  },[]);
 }
 
 export default useUpComingMovies;
